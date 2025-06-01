8702e2d27ee4ac5b2a263df4a875e0b5e2d2c366
Advik Aditya Singh <advikadityasingh@Adviks-MacBook-Air.local>
2025-06-01 11:40:31 -0500

    Chunking and Embeddings

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from services.pdf_parser import pdf_extraction
-from services.chunking import chunk_extracted_text
+from fastapi import FastAPI
+from router import process_pdf
 
+app = FastAPI()
 
-def main():
-    pdf_path = "Consitution of India.pdf"  
-    pages = pdf_extraction(pdf_path)
-    chunks = chunk_extracted_text(pages)
-
-    for chunk in chunks[:3]: 
-        print(f"\n[Page {chunk['page_number']} - Chunk {chunk['chunk_index']}]:")
-        print(chunk['content'][:300], "...")
-
-if __name__ == "__main__":
-    main()
+app.include_router(process_pdf.router)
---
+++ router/process_pdf.py
@@ -0,0 +1,23 @@
+from fastapi import HTTPException, APIRouter
+from services.pdf_parser import pdf_extraction
+from services.chunking import chunk_extracted_text
+from services.embeddings import chunk_storage
+import os
+
+router = APIRouter()
+
+pdf_path = "Consitution of India.pdf"
+
+@router.get("/process-pdf")
+async def process_local_pdf():
+    if not os.path.exists(pdf_path):
+        raise HTTPException(status_code=404, detail="PDF file not found.")
+
+    pages = pdf_extraction(pdf_path)
+    chunks = chunk_extracted_text(pages)
+    chunk_storage(chunks)
+
+    return {
+        "message": "PDF parsed, chunked, and stored in FAISS successfully.",
+        "chunks": chunks
+    }
